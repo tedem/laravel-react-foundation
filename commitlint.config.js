091a36f96374bdc5f194de403a8a48a90c21a078
Medet Erdal <tedem@tedem.dev>
2026-08-21 23:07:26 +0300

    chore: add custom commitlint rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'refactor', 'perf', 'docs', 'test', 'style', 'build', 'ci', 'chore']],
+    'header-max-length': [2, 'always', 50],
+  },
+};
